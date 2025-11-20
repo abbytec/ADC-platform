907023c8aa810291404ea76bf3795092420668c9
Abbytec <gpsmurfs@gmail.com>
2025-11-20 20:43:33 -0300

    remove some unused code and refactors

---
+++ globals.d.ts
@@ -0,0 +1,24 @@
+// Declaraciones globales para TypeScript
+// Esto hace que cualquier módulo que coincida con estos patrones sea válido
+
+// Module Federation - apps remotas
+declare module 'home/App' {
+  const App: React.ComponentType;
+  export default App;
+}
+
+declare module 'users-management/App' {
+  const App: React.ComponentType;
+  export default App;
+}
+
+declare module 'config/App' {
+  const App: React.ComponentType;
+  export default App;
+}
+
+// Wildcard para cualquier módulo .js
+declare module '*.js' {
+  const content: any;
+  export default content;
+}
