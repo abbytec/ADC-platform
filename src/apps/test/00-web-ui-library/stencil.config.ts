aae606335fdcda5daff5d213413adac98b40a443
Abbytec <gpsmurfs@gmail.com>
2025-11-21 20:06:08 -0300

    feat: Simplified stencil boilerplate

--- src/apps/test/00-web-ui-library/stencil.config.ts
+++ src/apps/test/00-web-ui-library/stencil.config.ts
@@ -1,24 +1,27 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-	namespace: 'web-ui-library',
-	outputTargets: [
-		{
-			type: 'dist',
-			dir: 'dist-ui',
-			esmLoaderPath: '../loader',
-		},
-		{
-			type: 'dist-custom-elements',
-			dir: 'dist-ui/dist-custom-elements',
-			customElementsExportBehavior: 'auto-define-custom-elements',
-			externalRuntime: false,
-		},
-		{
-			type: 'docs-readme',
-		},
-	],
-	sourceMap: true,
-	buildEs5: false,
+  "namespace": "web-ui-library",
+  "outputTargets": [
+    {
+      "type": "dist",
+      "dir": "../../../../temp/ui-builds/web-ui-library"
+    },
+    {
+      "type": "dist-custom-elements",
+      "dir": "../../../../temp/ui-builds/web-ui-library/custom-elements",
+      "customElementsExportBehavior": "auto-define-custom-elements",
+      "externalRuntime": false
+    },
+    {
+      "type": "docs-readme"
+    }
+  ],
+  "sourceMap": true,
+  "buildEs5": false,
+  "copy": [
+    {
+      "src": "utils"
+    }
+  ]
 };
-
