544c6609a5218f6dad95d1ba71d0249021a31155
Abbytec <gpsmurfs@gmail.com>
2025-12-15 01:20:29 -0300

    enhancement: footer flexibility

--- src/apps/public/00-adc-ui-library/src/components/organisms/adc-site-footer/adc-site-footer.tsx
+++ src/apps/public/00-adc-ui-library/src/components/organisms/adc-site-footer/adc-site-footer.tsx
@@ -9,26 +9,41 @@ export class AdcSiteFooter {
 	@Prop() brandSlogan: string = "";
 	@Prop() creatorName: string = "";
 	@Prop() creatorHref: string = "";
+	@Prop() lowerSign: boolean = false;
 
 	private getYear(): number {
 		return new Date().getFullYear();
 	}
 
+	signComponent() {
+		return (
+			<adc-text>
+				&copy; {this.getYear()} {this.brandName} - {this.brandSlogan} · creada por{" "}
+				<a
+					href={this.creatorHref}
+					target="_blank"
+					rel="noopener noreferrer"
+					aria-label={`Sitio de ${this.creatorName} (se abre en una pestaña nueva)`}
+				>
+					{this.creatorName}
+					<span class="sr-only"> (se abre en una pestaña nueva)</span>
+				</a>
+			</adc-text>
+		);
+	}
+
 	render() {
+		if (this.lowerSign) {
+			return (
+				<footer class="py-4 text-center opacity-80 border-t border-gray-200 flex-shrink-0 min-h-24 cv-auto">
+					<slot></slot>
+					{this.signComponent()}
+				</footer>
+			);
+		}
 		return (
 			<footer class="py-4 text-center opacity-80 border-t border-gray-200 flex-shrink-0 min-h-24 cv-auto ">
-				<adc-text>
-					&copy; {this.getYear()} {this.brandName} - {this.brandSlogan} · creada por{" "}
-					<a
-						href={this.creatorHref}
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label={`Sitio de ${this.creatorName} (se abre en una pestaña nueva)`}
-					>
-						{this.creatorName}
-						<span class="sr-only"> (se abre en una pestaña nueva)</span>
-					</a>
-				</adc-text>
+				{this.signComponent()}
 				<slot></slot>
 			</footer>
 		);
